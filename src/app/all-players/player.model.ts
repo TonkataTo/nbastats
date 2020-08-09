b4fd74bd1d534a08260083eb39d535ac6990a356
Antoni Tomov <antonitomovofficial@abv.bg>
2020-08-09 23:34:42 +0300

    Adding more stats for the Players and the Teams

--- src/app/all-players/player.model.ts
+++ src/app/all-players/player.model.ts
@@ -3,6 +3,11 @@ import { ITeam } from '../all-teams/team.model';
 export interface IPlayer {
     id?: any;
     first_name?: string;
+    last_name?: string;
+    position?: string;
+    height_feet?: number;
+    height_inches?: number;
+    weight_pounds?: number;
     team?: ITeam;
 }
 
@@ -11,6 +16,17 @@ export class Player implements IPlayer {
     public id?: any,
     // tslint:disable-next-line: variable-name
     public first_name?: string,
+    // tslint:disable-next-line: variable-name
+    public last_name?: string,
+    public position?: string,
+    // tslint:disable-next-line: variable-name
+    public height_feet?: number,
+    // eslint-disable-next-line @typescript-eslint/tslint/config
+    // tslint:disable-next-line: variable-name
+    public height_inches?: number,
+    // eslint-disable-next-line @typescript-eslint/tslint/config
+    // tslint:disable-next-line: variable-name
+    public weight_pounds?: number,
     public team?: ITeam
   ) {}
 }
--- src/app/all-teams/team.model.ts
+++ src/app/all-teams/team.model.ts
@@ -1,14 +1,22 @@
 export interface ITeam {
     id?: any;
     abbreviation?: string;
+    city?: string;
+    conference?: string;
+    division?: string;
     full_name?: string;
+    name?: string;
   }
 
 export class Team implements ITeam {
   constructor(
     public id?: any,
     public abbreviation?: string,
+    public city?: string,
+    public conference?: string,
+    public division?: string,
     // tslint:disable-next-line: variable-name
-    public full_name?: string
+    public full_name?: string,
+    public name?: string
   ) {}
 }
